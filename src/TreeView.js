73414f5100fe70416ba03ada01a45c32990c7ae1
Michael Fry <michaelfry2002@gmail.com>
2014-11-03 17:07:14 +1300

    Make API more betterer

--- src/TreeView.js
+++ src/TreeView.js
@@ -26,6 +26,20 @@
 		this.root = new TreeViewNode(data, options);
 	}
 
+	ko.bindingHandlers.treeView = {
+		init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+			var root = allBindings().treeView.root,
+				internalList = document.createElement("ul");
+			internalList.style["list-style-type"] = "none";
+			internalList.style["margin-top"] = "0px";
+			internalList.style["margin-top"] = "0px";
+			internalList.style.padding = "0px";
+			element.appendChild(internalList);
+			ko.applyBindingsToNode(internalList, { template: { name: 'tree-node', data: root } });
+			return { controlsDescendantBindings: true };
+		}
+	};
+
 	return TreeView;
 
 });
